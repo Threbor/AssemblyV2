5b1d5f0b74e8b6539844ca71f221622eb98823b0
Hugo ROBERT <hugo.pierre.robert@gmail.com>
2023-06-28 16:41:43 +0200

    Possible to select a universe and create a storyroom from it + Seeds Added

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,9 +24,42 @@ puts 'DB Cleaned!'
 
 puts ''
 puts 'Creating Universes...'
-Universe.create(title: 'Glaciarift')
-Universe.create(title: 'Foragreen')
-Universe.create(title: 'Desaride')
+Universe.create(
+  title: 'Glaciarift',
+  description: 'Un univers de glace et de neige'
+)
+Universe.create(
+  title: 'Foragreen',
+  description: 'Un univers de forêt et de verdure'
+)
+Universe.create(
+  title: 'Desaride',
+  description: 'Un univers de désert et de sable'
+)
+Universe.create(
+  title: 'Blocean',
+  description: 'Un univers d\'eau et d\'ilots'
+)
+Universe.create(
+  title: 'Aeris',
+  description: 'Un univers de vent et de nuages'
+)
+Universe.create(
+  title: 'Vulcania',
+  description: 'Un univers de feu et de lave'
+)
+Universe.create(
+  title: 'Abyssia',
+  description: 'Un univers de ténèbres et de vide'
+)
+Universe.create(
+  title: 'Aetheria',
+  description: 'Un univers de lumière et de vide'
+)
+Universe.create(
+  title: 'Aurora',
+  description: 'Un univers de lumière et de glace'
+)
 puts 'Universes created!'
 
 
@@ -36,23 +69,56 @@ Word.create(title: 'Espoire')
 Word.create(title: 'Trahison')
 Word.create(title: 'Amour')
 Word.create(title: 'Avalanche')
+Word.create(title: 'Rencontre')
+Word.create(title: 'Trahison')
+Word.create(title: 'Découverte')
+Word.create(title: 'Victoire')
+Word.create(title: 'Défaite')
+Word.create(title: 'Naissance')
+Word.create(title: 'Mort')
+Word.create(title: 'Mariage')
+Word.create(title: 'Divorce')
+Word.create(title: 'Révélation')
+Word.create(title: 'Conflit')
+Word.create(title: 'Résolution')
 puts 'words created!'
-
 
 puts ''
 puts 'Creating Characters...'
-Character.create(name: 'Olaf le viking')
+Character.create(
+  name: 'Olaf le viking',
+  quotation: 'Harg!',
+  background: 'Né dans les montagnes enneigées de la Norvège, Olaf a grandi dans un village de fiers guerriers vikings. Il a appris à chasser et à combattre dès son plus jeune âge. Son père, un chef de clan respecté, lui a enseigné l\'art de la guerre, tandis que sa mère, une voyante, lui a transmis les traditions et les mythes de son peuple.
+  Intrépide et audacieux, Olaf rêvait d\'explorer le monde bien au-delà de son village natal. Un jour, alors qu\'il n\'était qu\'un adolescent, il prit la mer avec un groupe de vikings. Depuis, il a navigué sur les mers inconnues, a combattu des bêtes mythiques, et a pillé de riches cités.
+  Malgré sa nature sauvage, Olaf est connu pour sa grande loyauté. Il ne recule devant rien pour protéger son clan et ceux qu\'il considère comme sa famille. "Harg!", son cri de guerre, est devenu un symbole de courage et de détermination parmi son peuple.'
+)
+Character.create(
+  name: 'Erug',
+  quotation: 'Les profondeurs détiennent des secrets que l\'homme ne peut comprendre.',
+  background: 'Né de l\'union d\'une sirène et d\'un marin, Erug incarne la frontière entre les hommes et les créatures marines. Ses écailles luisantes et sa capacité à respirer sous l\'eau témoignent de son héritage marin, tandis que son humanité se manifeste dans son désir de paix entre ces deux mondes. Sa connaissance profonde de la mer et de ses secrets lui permet d\'être un ambassadeur naturel, travaillant sans relâche à la coexistence pacifique entre les êtres humains et les habitants des profondeurs.'
+)
+
 puts 'Characters created!'
 
 puts ''
 puts 'Creating Universe_character_associations...'
+puts 'Creating Universe_character_associations... for Glaciarift'
 UniverseCharacterAssociation.create(
   universe_id: Universe.find_by(title: 'Glaciarift').id,
   character_id: Character.find_by(name: 'Olaf le viking').id)
+puts 'Creating Universe_character_associations... for Glaciarift done!'
+
+puts 'Creating Universe_character_associations... for Blocean'
+UniverseCharacterAssociation.create(
+  universe_id: Universe.find_by(title: 'Blocean').id,
+  character_id: Character.find_by(name: 'Erug').id)
+puts 'Creating Universe_character_associations... for Blocean done!'
+
 puts 'Universe_character_associations created!'
 
 puts ''
 puts 'Creating Universe_word_associations...'
+puts 'Creating Universe_word_associations... for Glaciarift'
 UniverseWordAssociation.create(
   universe_id: Universe.find_by(title: 'Glaciarift').id,
   word_id: Word.find_by(title: 'Espoire').id
@@ -69,6 +135,39 @@ UniverseWordAssociation.create(
   universe_id: Universe.find_by(title: 'Glaciarift').id,
   word_id: Word.find_by(title: 'Avalanche').id
 )
+puts 'Creating Universe_word_associations... for Glaciarift done!'
+
+puts 'Creating Universe_word_associations... for Blocean'
+UniverseWordAssociation.create(
+  universe_id: Universe.find_by(title: 'Blocean').id,
+  word_id: Word.find_by(title: 'Rencontre').id
+)
+UniverseWordAssociation.create(
+  universe_id: Universe.find_by(title: 'Blocean').id,
+  word_id: Word.find_by(title: 'Trahison').id
+)
+UniverseWordAssociation.create(
+  universe_id: Universe.find_by(title: 'Blocean').id,
+  word_id: Word.find_by(title: 'Découverte').id
+)
+UniverseWordAssociation.create(
+  universe_id: Universe.find_by(title: 'Blocean').id,
+  word_id: Word.find_by(title: 'Victoire').id
+)
+UniverseWordAssociation.create(
+  universe_id: Universe.find_by(title: 'Blocean').id,
+  word_id: Word.find_by(title: 'Défaite').id
+)
+UniverseWordAssociation.create(
+  universe_id: Universe.find_by(title: 'Blocean').id,
+  word_id: Word.find_by(title: 'Naissance').id
+)
+UniverseWordAssociation.create(
+  universe_id: Universe.find_by(title: 'Blocean').id,
+  word_id: Word.find_by(title: 'Mort').id
+)
+puts 'Creating Universe_word_associations... for Blocean done!'
+
 puts 'Universe_word_associations created!'
 
 puts ''
