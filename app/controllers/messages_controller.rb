b68b59c47ca1b20bb297f9104f3ad4b720644920
Hugo ROBERT <hugo.pierre.robert@gmail.com>
2023-07-05 12:27:26 +0200

    Well advanced features

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,18 +3,19 @@ class MessagesController < ApplicationController
     @storyroom = Storyroom.find(params[:storyroom_id])
     @message = @storyroom.messages.build(message_params)
     @message.user = current_user
+    # raise
+    @universe = @storyroom.universe
+    @timeline_items = (@storyroom.events + @storyroom.messages).reject { |item| item.created_at.nil? }.sort_by(&:created_at).reverse
+    @storyroom_characters = @storyroom.storyroom_characters
+    @storycards = @storyroom.storycards
+    @characters = @storyroom.universe.characters
+    @events = @storyroom.events
+    @words = @storyroom.universe.words
 
     if @message.save
       redirect_to @storyroom
     else
-      @universe = @storyroom.universe
-      @timeline_items = (@storyroom.events + @storyroom.messages).reject { |item| item.created_at.nil? }.sort_by(&:created_at).reverse
-      @storyroom_character = @storyroom.storyroom_characters.first
-      @storycards = @storyroom.storycards
-      @characters = @storyroom.universe.characters
-      @events = @storyroom.events
-      @words = @storyroom.universe.words
-      
+
       render 'storyrooms/show'
     end
   end
--- app/controllers/storyroom_characters_controller.rb
+++ app/controllers/storyroom_characters_controller.rb
@@ -5,16 +5,17 @@ class StoryroomCharactersController < ApplicationController
     @universe = @storyroom.universe
     @characters = @universe.characters
     @storyroom_character = StoryroomCharacter.new
-    @close_users = []
+    @close_users = Set.new
     current_user.storyroom_characters.each do |storyroom_character|
-      @close_users << User.find(storyroom_character.storyroom.user_id) if User.find(storyroom_character.storyroom.user_id) != current_user
+      user = User.find(storyroom_character.storyroom.user_id)
+      @close_users << user if user != current_user
     end
-
   end
 
   def create
     @storyroom = Storyroom.find(params[:storyroom_id])
     @user = User.find_by(username: params[:username])
+
     @character = Character.find(params[:character_id])
     @storyroom_character = StoryroomCharacter.new
     @storyroom_character.storyroom = @storyroom
@@ -26,6 +27,12 @@ class StoryroomCharactersController < ApplicationController
     if @storyroom_character.save
       redirect_to storyroom_path(@storyroom)
     else
+      @universe = @storyroom.universe
+      @characters = @universe.characters
+      @close_users = []
+      current_user.storyroom_characters.each do |storyroom_character|
+        @close_users << User.find(storyroom_character.storyroom.user_id) if User.find(storyroom_character.storyroom.user_id) != current_user
+      end
       render :new
     end
   end
@@ -37,23 +44,45 @@ class StoryroomCharactersController < ApplicationController
   end
 
   def update
-    @storyroom = Storyroom.find(params[:storyroom_id])
     @storyroom_character = StoryroomCharacter.find(params[:id])
-    @storyroom_character.update(storyroom_character_params)
+    @storyroom_character.update(
+      name: storyroom_character_params[:storyroom_character_name],
+      quotation: storyroom_character_params[:storyroom_character_quotation],
+      background: storyroom_character_params[:storyroom_character_background]
+    )
+    @storyroom = Storyroom.find(@storyroom_character.storyroom_id)
     redirect_to storyroom_path(@storyroom)
   end
+
 
   def destroy
     @storyroom = Storyroom.find(params[:storyroom_id])
     @storyroom_character = StoryroomCharacter.find(params[:id])
+    @storyroom_character_user = @storyroom_character.user
+    @storyroom_user_messages = @storyroom.messages.where( user: @storyroom_character_user)
+    @storyroom_user_messages.each do |message|
+      message.destroy
+    end
     @storyroom_character.destroy
-    redirect_to storyroom_path(@storyroom)
+
+    if current_user == @storyroom.user
+      redirect_to storyroom_path(@storyroom)
+    else
+      redirect_to storyrooms_path
+    end
   end
 
   private
 
   def storyroom_character_params
-    params.require(:storyroom_storyroom_characters).permit(:storyroom_id, :character_id, :username)
+    params.require(:storyroom_character).permit(
+      :storyroom_id,
+      :character_id,
+      :username,
+      :storyroom_character_name,
+      :storyroom_character_quotation,
+      :storyroom_character_background,
+    )
   end
 
 end
